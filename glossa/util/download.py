293c334bce64cd3d3a80c593c503827ad0c9cff7
abhishtagatya <abhishtagatya@yahoo.com>
2023-09-05 15:55:22 +0700

    Add Download Pretrained from Github and Logging

--- glossa/util/download.py
+++ glossa/util/download.py
@@ -1,12 +1,38 @@
+import logging
 import os
 
+import requests
 import kaggle
 
 
 def download_kaggle_dataset(dataset: str, data_dir: str = '.data/'):
-    get_dir = os.getenv('GLOSSA_DATA', data_dir)
-    if not os.path.exists(get_dir):
-        os.mkdir(get_dir)
+    sub_folder = '/dataset'
+    full_dir = os.getenv('GLOSSA_DATA', data_dir) + sub_folder
 
-    kaggle.api.dataset_download_files(dataset, data_dir, unzip=True)
+    if not os.path.exists(full_dir):
+        os.mkdir(full_dir)
+
+    logging.info(f'Fetching Dataset {dataset} from Kaggle.')
+    kaggle.api.dataset_download_files(dataset, full_dir, unzip=True)
     return
+
+
+def download_github_pretrained(url: str, filename: str, data_dir: str = '.data/'):
+    sub_folder = '/pretrained'
+    full_dir = os.getenv('GLOSSA_DATA', data_dir) + sub_folder
+
+    if not os.path.exists(full_dir):
+        os.mkdir(full_dir)
+
+    if not os.path.exists(full_dir + filename):
+        logging.info(f'Fetching Pretrained {filename} from Github.')
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(full_dir + filename, 'wb') as file:
+                file.write(response.content)
+        else:
+            return ''
+    return full_dir + filename
+
+
+
